abf0af9d4ab2738cd2b351c2b4e09da09ef45fd8
Ryo Shinozaki <shinozaki@fullmarks.co.jp>
2025-07-17 16:38:53 +0900

    - ポケモンアイコンSVGを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,48 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Play, Trophy } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="min-h-screen text-center text-white bg-gradient-to-br from-sky-400 from-0% via-violet-600 via-50% to-violet-800 to-100%">
-      <div className="max-w-3xl mx-auto py-20">
-        <p className="font-bold text-5xl mb-10 pt-2">ポケモンクイズ</p>
+    <div className="min-h-screen text-white bg-gradient-to-br from-sky-400 from-0% via-violet-600 via-50% to-violet-800 to-100%">
+      <div className="max-w-3xl mx-auto py-20 text-center">
+        <div className="flex justify-center gap-3 mb-10">
+          <Image
+            width={50}
+            height={50}
+            src="icons/pokeball-pokemon-svgrepo-com.svg"
+            alt="pokemon ball"
+          ></Image>
+          <p className="font-bold text-5xl">ポケモンクイズ</p>
+        </div>
         <p className="text-2xl text-yellow-200 pb-10">
           あなたのポケモン知識を試そう！
           <br></br>
           初代から最新作まで、幅広い問題でトレーナーレベルをチェック
         </p>
-        <Button
-          className="bg-amber-300 text-gray-800 hover:bg-amber-100"
-          size="lg"
-          onClick={() => router.push("/quiz")}
-        >
-          クイズを始める
-        </Button>
+        <div className="flex justify-center gap-4">
+          <Button
+            className="bg-amber-300 text-amber-900 hover:bg-amber-100 font-bold"
+            size="lg"
+            onClick={() => router.push("/quiz")}
+          >
+            <Play></Play>
+            クイズを始める
+          </Button>
+          <Button
+            className="bg-amber-300 text-amber-900 hover:bg-amber-100 font-bold"
+            size="lg"
+            onClick={() => router.push("/ranking")}
+          >
+            <Trophy></Trophy>
+            ランキングを見る
+          </Button>
+        </div>
       </div>
     </div>
   );
